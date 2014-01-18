02c3f0c472fce4e493092e3e980a78e050cb4bfe
Robert Graves <robertjgraves@gmail.com>
2014-01-18 12:28:38 -0800

    initial commit of Wikiful rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikiful::Application.config.secret_key_base = '1a7ce7b7b3f36556693ec3447f043e3a4e2d0b3a69ddc2b70190e7e4f9a0dc77d9930f0d58338ed13dee82b1856cfd8f28ea970519259d87566bee735c57555a'
