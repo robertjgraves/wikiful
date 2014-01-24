89f424399124e05ad09f184ffd6f5602ddf44fbd
Robert Graves <robertjgraves@gmail.com>
2014-01-23 16:28:40 -0800

    user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :articles
+  validates_uniqueness_of :email
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
