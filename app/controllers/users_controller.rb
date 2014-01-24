badbf9ace3eb71ddc57a638cefe537c00d3053b0
Robert Graves <robertjgraves@gmail.com>
2014-01-23 16:57:10 -0800

    style new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user),permit(:email, :first_name, :last_name, :password, :password_confirmation)
+      params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
     end
 end
