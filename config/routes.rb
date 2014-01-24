d8496a53219edd97af65d68b0c5db9356b2b178f
Robert Graves <robertjgraves@gmail.com>
2014-01-23 20:45:44 -0800

    add sessions form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Wikiful::Application.routes.draw do
-  resources :users
-
+  
   get "welcome/index"
     root 'welcome#index'
 
   resources :articles
   resources :categories
   resources :users
+  resources :sessions
 end
